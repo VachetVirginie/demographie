001a6bec401e5c0f83eeaa04b8569a36b9d6377f
Vachet Virginie <vachetvirginie@gmail.com>
2018-08-30 16:58:42 +0200

    Composant du drapeau

---
+++ src/components/mortality-list-item.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Flag from './flag'
+
+
+const MortalityListItem = () => {
+    return (
+        <div>
+              <Flag country={"Argentina"} className="flag_medium"/>
+        </div>
+    )
+}
+
+export default MortalityListItem
